ad45fac91af13b28e038dd2e512965a0829532a2
Victor Hugo Barajas Lamus <victorhugo877@gmail.com>
2024-03-07 21:07:51 -0500

    Project init angular 16
    angular 16 initial configuration files

---
+++ project0001-angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbAlertModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
